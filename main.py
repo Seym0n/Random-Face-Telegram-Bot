943dce03ce22c0168a4cdd7d6d01287a889e415b
Simon <sschol24@smail.uni-koeln.de>
2022-12-31 23:47:44 +0100

    Improve patterns in code

--- main.py
+++ main.py
@@ -7,19 +7,21 @@ from PIL import Image
 
 load_dotenv()
 
-TG_TOKEN = os.getenv('TG_TOKEN')
+TG_TOKEN = os.getenv("TG_TOKEN")
 
-bot = telebot.TeleBot(TG_TOKEN, parse_mode='MARKDOWN') # You can set parse_mode by default. HTML or MARKDOWN
+bot = telebot.TeleBot(TG_TOKEN, parse_mode='MARKDOWN')
 
 standard_message = "Hi, I'll will send an artifical generated face if you send me a /generate command"
 
 @bot.message_handler(commands=['generate'])
 def send_random_face(message):
-	random_photo = Image.open(requests.get('https://thispersondoesnotexist.com/image', stream = True).raw)
-	bot.send_photo(message.chat.id, random_photo)
+    random_photo = Image.open(requests.get('https://thispersondoesnotexist.com/image', stream=True).raw)
+    bot.send_photo(message.chat.id, random_photo)
+
 
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
-	bot.send_message(message.chat.id, standard_message)
+    bot.send_message(message.chat.id, standard_message)
+
 
 bot.infinity_polling()
