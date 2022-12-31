f2c3ad418a584029315a9ef5841e1388a4a704ca
Simon <sschol24@smail.uni-koeln.de>
2022-12-31 23:51:43 +0100

    Change main.py

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ bot = telebot.TeleBot(TG_TOKEN, parse_mode='MARKDOWN')
 
 standard_message = "Hi, I'll will send an artifical generated face if you send me a /generate command"
 
+
 @bot.message_handler(commands=['generate'])
 def send_random_face(message):
     random_photo = Image.open(requests.get('https://thispersondoesnotexist.com/image', stream=True).raw)
