829de8cc0589c887590de317532df5d9b7c90df2
Simon <sschol24@smail.uni-koeln.de>
2022-12-31 23:19:51 +0100

    Add files

---
+++ main.py
@@ -0,0 +1,24 @@
+import telebot
+import os
+import requests
+from dotenv import load_dotenv
+from PIL import Image
+
+load_dotenv()
+
+TG_TOKEN = os.getenv('TG_TOKEN')
+
+bot = telebot.TeleBot(TG_TOKEN, parse_mode='MARKDOWN') # You can set parse_mode by default. HTML or MARKDOWN
+
+standard_message = "Hi, I'll will send an artifical generated face if you send me a /generate command"
+
+@bot.message_handler(commands=['generate'])
+def send_random_face(message):
+	random_photo = Image.open(requests.get('https://thispersondoesnotexist.com/image', stream = True).raw)
+	bot.send_photo(message.chat.id, random_photo)
+
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+	bot.send_message(message.chat.id, standard_message)
+
+bot.infinity_polling()
