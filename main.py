d44f047cf4c6c9191ae4708f9a871ea38395e5f6
Simon <sschol24@smail.uni-koeln.de>
2023-01-01 01:20:41 +0100

    Change main.py

--- main.py
+++ main.py
@@ -9,14 +9,18 @@ load_dotenv()
 
 TG_TOKEN = os.getenv("TG_TOKEN")
 
-bot = telebot.TeleBot(TG_TOKEN, parse_mode='MARKDOWN')
+bot = telebot.TeleBot(TG_TOKEN, parse_mode="MARKDOWN")
 
-standard_message = "Hi, I'll will send an artifical generated face if you send me a /generate command"
+standard_message = (
+    "Hi, I'll will send an artifical generated face if you send me a /generate command"
+)
 
 
 @bot.message_handler(commands=['generate'])
 def send_random_face(message):
-    random_photo = Image.open(requests.get('https://thispersondoesnotexist.com/image', stream=True).raw)
+    random_photo = Image.open(
+        requests.get("https://thispersondoesnotexist.com/image", stream=True).raw
+    )
     bot.send_photo(message.chat.id, random_photo)
 
 
